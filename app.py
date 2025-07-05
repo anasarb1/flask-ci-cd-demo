aa02a1ca7ad811491dd757e5c107360338ed0b42
anas arab <ans123123@hotmail.com>
2025-07-05 19:27:49 +0100

    Working Flask Docker setup

---
+++ app.py
@@ -0,0 +1,13 @@
+﻿from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "IT'S WORKING!"
+
+@app.route("/health")
+def health():
+    return {"status": "healthy"}, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
